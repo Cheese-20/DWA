31e455825b04a69c7bf5de140bb97f5d503d887a
Cheese-20 <ssgamane64@gmail.com>
2023-08-23 13:30:36 +0200

    Completed DWA5

--- DWA5/script.js
+++ DWA5/script.js
@@ -6,19 +6,18 @@ form.addEventListener("submit", (event) => {
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
 
-  if (dividend =='' || divider == ''){
-    result.innerText = 'Division not performed. Both values are required in inputs. Try again';
-  } 
-
+  
   if(divider < 0){
     result.innerText = 'Division not performed. Invalid number provided. Try again'; 
-    console.error(error.stack);
-}
+    console.error(error.stack);}
 
  try{
-    result.innerText = Math.round(dividend / divider);
-        }catch(err){
-            document.querySelector('body').innerText = 'Something critical went wrong. Please reload the page';
+    if (dividend =='' || divider == ''){
+        result.innerText = 'Division not performed. Both values are required in inputs. Try again';} 
+        else{result.innerText = Math.round(dividend / divider);}
+        }
+        catch(err){
+            document.querySelector('body').innerHTML = 'Something critical went wrong. Please reload the page';
 }
     
 });
