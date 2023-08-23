02209713c0ebc709cf980acbe8a663a6db891e25
Cheese-20 <ssgamane64@gmail.com>
2023-08-23 12:59:56 +0200

    completed DWA5

--- DWA5/script.js
+++ DWA5/script.js
@@ -2,21 +2,23 @@ const form = document.querySelector("[data-form]");
 const result = document.querySelector("[data-result]");
 
 form.addEventListener("submit", (event) => {
-  event.preventDefault();
+    event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
-  
+
   if (dividend =='' || divider == ''){
-    result.innerText = 'Division not performed. Both values are required in inputs. Try again'
-  } else { 
-    try{
-        result.innerText = Math.round(dividend / divider);
-      } catch{err}(
-        document.querySelector('body').innerText = 'Something critical went wrong. Please reload the page'
-      )
-  }
-        if(divider < 0){
-            result.innerText = 'Division not performed. Invalid number provided. Try again'; 
-            console.error(error.stack);
-      }
-    });
+    result.innerText = 'Division not performed. Both values are required in inputs. Try again';
+  } 
+
+  if(divider < 0){
+    result.innerText = 'Division not performed. Invalid number provided. Try again'; 
+    console.error(error.stack);
+}
+
+ try{
+    result.innerText = Math.round(dividend / divider);
+        }catch(err){
+            document.querySelector('body').innerText = 'Something critical went wrong. Please reload the page';
+}
+    
+});
