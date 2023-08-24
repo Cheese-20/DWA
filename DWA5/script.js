c12e47f098d9bd863c8954dbf29b037193a04ec8
Cheese-20 <ssgamane64@gmail.com>
2023-08-24 18:05:48 +0200

    DWA5

--- DWA5/script.js
+++ DWA5/script.js
@@ -2,22 +2,32 @@ const form = document.querySelector("[data-form]");
 const result = document.querySelector("[data-result]");
 
 form.addEventListener("submit", (event) => {
-    event.preventDefault();
+  event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
 
   
-  if(divider < 0){
-    result.innerText = 'Division not performed. Invalid number provided. Try again'; 
-    console.error(error.stack);}
-
- try{
-    if (dividend =='' || divider == ''){
-        result.innerText = 'Division not performed. Both values are required in inputs. Try again';} 
-        else{result.innerText = Math.round(dividend / divider);}
-        }
-        catch(err){
-            document.querySelector('body').innerHTML = 'Something critical went wrong. Please reload the page';
-}
+  try {
     
+    if (dividend == "" || divider == "") {
+      result.innerText =
+        "Division not performed. Both values are required in inputs. Try again";
+    }
+  
+    if (divider < 0) {
+      result.innerText =
+        "Division not performed. Invalid number provided. Try again";
+      throw new Error();
+    }
+
+    if (typeof divider !== Number || typeof dividend !== Number) {
+      result.innerText =
+        "Something critical went wrong. Please reload the page";
+      throw new Error();
+    }
+
+    result.innerHTML = Math.round(dividend/divider);
+  } catch (err) {
+    console.error(err);
+  }
 });
