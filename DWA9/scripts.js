45071c82f809d15f0e7636da2996b2d0043eff2d
Cheese-20 <ssgamane64@gmail.com>
2023-09-10 16:38:41 +0200

    created the button

--- DWA9/scripts.js
+++ DWA9/scripts.js
@@ -72,9 +72,49 @@ const createElement = (author, id, image, title) => {
 // template
 const template =  document.createElement('template');
 template.innerHTML =
-`<button>
+`<style>
+.preview {
+  border-width: 0;
+  width: 100%;
+  font-family: Roboto, sans-serif;
+  padding: 0.5rem 1rem;
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  text-align: left;
+  border-radius: 8px;
+  border: 1px solid rgba(var(--color-dark), 0.15);
+  background: rgba(var(--color-light), 1);
+}
+.preview__image {
+  width: 48px;
+  height: 70px;
+  object-fit: cover;
+  background: grey;
+  border-radius: 2px;
+  box-shadow: 0px 2px 1px -1px rgba(0, 0, 0, 0.2),
+    0px 1px 1px 0px rgba(0, 0, 0, 0.1), 0px 1px 3px 0px rgba(0, 0, 0, 0.1);
+}
+
+.preview__title {
+  margin: 0 0 0.5rem;
+  font-weight: bold;
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;  
+  overflow: hidden;
+  color: rgba(var(--color-dark), 0.8)
+}
+.preview__author {
+  color: rgba(var(--color-dark), 0.4);
+}
+</style>
+
+
+<button class="preview">
     <img
-      class="preview__image" src="" data-images/>
+      class="preview__image" data-images
+        src="{image}"/>
           
     <div class="preview__info">
           <h3 class="preview__title" data-titles></h3>
@@ -128,16 +168,13 @@ class customPreviewHtml extends HTMLElement {
           authors : this.shadow.querySelector('[data-authors]')
         };
     
-        this.#elements.authors.innerText = this.#author ;
+        this.#elements.authors.innerHTML = this.#author ;
         this.#elements.images.innerText = this.#image ;
-        this.#elements.titles.innerText = this.#title;
+        this.#elements.titles.innerHTML = this.#title;
       }
-    
 }
 
  customElements.define("preview-books", customPreviewHtml);
-
- 
 
 /**
  * Fragments for the search and filter options being created
