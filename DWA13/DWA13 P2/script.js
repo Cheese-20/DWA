3b849a0de95d49ef9cb2ffd4cd342e6853fa0314
Cheese-20 <ssgamane64@gmail.com>
2023-09-20 14:06:25 +0200

    DWA 13

--- DWA13/DWA13 P2/script.js
+++ DWA13/DWA13 P2/script.js
@@ -37,25 +37,36 @@ console.log(totalPrice);
 
 const concat = products.reduce((word ,item)=>{
     return `${word},${item.product}`
-},'')
+},'');
 
 console.log(concat);
 
-// TODO Highest and lowest 
-const prices = products
-  .filter((product) => product.price !== '' && !isNaN(parseFloat(product.price)))
-  .map((product) => parseFloat(product.price));
+// TODO: Display Highest product 
+const highestPrice = products.reduce((previous, current, index) => {
 
-const highestPrice = Math.max(...prices);
-const lowestPrice = Math.min(...prices);
-const highestProduct = products.find((product) => parseFloat(product.price) === highestPrice).product;
-const lowestProduct = products.find((product) => parseFloat(product.price) === lowestPrice).product;
-console.log(`Highest: ${highestProduct}. Lowest: ${lowestProduct}`);
+  if ( parseInt(current.price) > previous) {
+    return current.price;
+  } else {
+    return previous;
+  }
+  },0);
+
+console.log(`highest: ${highestPrice}`);
+
+const lowestPrice = products.reduce((previous, current) => {
+  if (current.price !== '' && parseInt(current.price) < previous) {
+    return current.product;
+  } else {
+    return previous;
+  }
+});
+
+console.log(`lowest: ${lowestPrice.product}`)
+
 
 // TODO New object
-const modifiedObject = Object.entries(products).reduce((obj, [key, value]) => {
-  const modifiedKey = key === 'product' ? 'name' : 'cost';
-  obj[modifiedKey] = value;
-  return obj;
+
+const sObject = Object.entries(products).reduce((obj, [key, value]) => {
+
 }, {});
-console.log(modifiedObject);
+console.log(sobj);
